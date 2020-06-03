042a34786d6829258814937adf98a26a618422f4
wangmi <87730066>
2020-06-03 09:32:49 +0800

    3.6.7 代码调整，不再依赖Swagger

--- sharp-sword/src/main/java/com/dili/ss/quartz/controller/ScheduleJobController.java
+++ sharp-sword/src/main/java/com/dili/ss/quartz/controller/ScheduleJobController.java
@@ -3,10 +3,6 @@ package com.dili.ss.quartz.controller;
 import com.dili.ss.domain.BaseOutput;
 import com.dili.ss.quartz.domain.ScheduleJob;
 import com.dili.ss.quartz.service.ScheduleJobService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
-import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,7 +19,7 @@ import java.util.List;
  * 由MyBatis Generator工具自动生成
  * This file was generated on 2017-10-24 09:32:32.
  */
-@Api("/scheduleJob")
+//@Api("/scheduleJob")
 @Controller
 @RequestMapping("/scheduleJob")
 @ConditionalOnProperty(name = "quartz.enabled")
@@ -44,34 +40,34 @@ public class ScheduleJobController {
 		return BaseOutput.success("调度器刷新完成");
 	}
 
-    @ApiOperation("跳转到ScheduleJob页面")
+//    @ApiOperation("跳转到ScheduleJob页面")
     @RequestMapping(value="/index.html", method = RequestMethod.GET)
     public String index(ModelMap modelMap) {
         return "scheduleJob/index";
     }
 
-    @ApiOperation(value="查询ScheduleJob", notes = "查询ScheduleJob，返回列表信息")
-    @ApiImplicitParams({
-		@ApiImplicitParam(name="ScheduleJob", paramType="form", value = "ScheduleJob的form信息", required = false, dataType = "string")
-	})
+//    @ApiOperation(value="查询ScheduleJob", notes = "查询ScheduleJob，返回列表信息")
+//    @ApiImplicitParams({
+//		@ApiImplicitParam(name="ScheduleJob", paramType="form", value = "ScheduleJob的form信息", required = false, dataType = "string")
+//	})
     @RequestMapping(value="/list.action", method = {RequestMethod.GET, RequestMethod.POST})
     public @ResponseBody List<ScheduleJob> list(ScheduleJob scheduleJob) {
         return scheduleJobService.list(scheduleJob);
     }
 
-    @ApiOperation(value="分页查询ScheduleJob", notes = "分页查询ScheduleJob，返回easyui分页信息")
-    @ApiImplicitParams({
-		@ApiImplicitParam(name="ScheduleJob", paramType="form", value = "ScheduleJob的form信息", required = false, dataType = "string")
-	})
+//    @ApiOperation(value="分页查询ScheduleJob", notes = "分页查询ScheduleJob，返回easyui分页信息")
+//    @ApiImplicitParams({
+//		@ApiImplicitParam(name="ScheduleJob", paramType="form", value = "ScheduleJob的form信息", required = false, dataType = "string")
+//	})
     @RequestMapping(value="/listPage.action", method = {RequestMethod.GET, RequestMethod.POST})
     public @ResponseBody String listPage(ScheduleJob scheduleJob) throws Exception {
         return scheduleJobService.listEasyuiPageByExample(scheduleJob, true).toString();
     }
 
-    @ApiOperation("新增ScheduleJob")
-    @ApiImplicitParams({
-		@ApiImplicitParam(name="ScheduleJob", paramType="form", value = "ScheduleJob的form信息", required = true, dataType = "string")
-	})
+//    @ApiOperation("新增ScheduleJob")
+//    @ApiImplicitParams({
+//		@ApiImplicitParam(name="ScheduleJob", paramType="form", value = "ScheduleJob的form信息", required = true, dataType = "string")
+//	})
     @RequestMapping(value="/insert.action", method = {RequestMethod.GET, RequestMethod.POST})
     public @ResponseBody
     BaseOutput insert(ScheduleJob scheduleJob) {
@@ -79,10 +75,10 @@ public class ScheduleJobController {
         return BaseOutput.success("新增成功");
     }
 
-    @ApiOperation("修改ScheduleJob")
-    @ApiImplicitParams({
-		@ApiImplicitParam(name="ScheduleJob", paramType="form", value = "ScheduleJob的form信息", required = true, dataType = "string")
-	})
+//    @ApiOperation("修改ScheduleJob")
+//    @ApiImplicitParams({
+//		@ApiImplicitParam(name="ScheduleJob", paramType="form", value = "ScheduleJob的form信息", required = true, dataType = "string")
+//	})
     @RequestMapping(value="/update.action", method = {RequestMethod.GET, RequestMethod.POST})
     public @ResponseBody
     BaseOutput update(ScheduleJob scheduleJob) {
@@ -90,10 +86,10 @@ public class ScheduleJobController {
         return BaseOutput.success("修改成功");
     }
 
-    @ApiOperation("删除ScheduleJob")
-    @ApiImplicitParams({
-		@ApiImplicitParam(name="id", paramType="form", value = "ScheduleJob的主键", required = true, dataType = "long")
-	})
+//    @ApiOperation("删除ScheduleJob")
+//    @ApiImplicitParams({
+//		@ApiImplicitParam(name="id", paramType="form", value = "ScheduleJob的主键", required = true, dataType = "long")
+//	})
     @RequestMapping(value="/delete.action", method = {RequestMethod.GET, RequestMethod.POST})
     public @ResponseBody
     BaseOutput delete(Long id) {
@@ -101,10 +97,10 @@ public class ScheduleJobController {
         return BaseOutput.success("删除成功");
     }
 
-	@ApiOperation("暂停ScheduleJob")
-	@ApiImplicitParams({
-			@ApiImplicitParam(name="scheduleJob", paramType="form", value = "ScheduleJob的主键", required = true)
-	})
+//	@ApiOperation("暂停ScheduleJob")
+//	@ApiImplicitParams({
+//			@ApiImplicitParam(name="scheduleJob", paramType="form", value = "ScheduleJob的主键", required = true)
+//	})
 	@RequestMapping(value="/pause.action", method = {RequestMethod.GET, RequestMethod.POST})
 	public @ResponseBody
 	BaseOutput pause(ScheduleJob scheduleJob) {
@@ -112,10 +108,10 @@ public class ScheduleJobController {
 		return BaseOutput.success("暂停成功");
 	}
 
-	@ApiOperation("恢复ScheduleJob")
-	@ApiImplicitParams({
-			@ApiImplicitParam(name="scheduleJob", paramType="form", value = "ScheduleJob的主键", required = true)
-	})
+//	@ApiOperation("恢复ScheduleJob")
+//	@ApiImplicitParams({
+//			@ApiImplicitParam(name="scheduleJob", paramType="form", value = "ScheduleJob的主键", required = true)
+//	})
 	@RequestMapping(value="/resume.action", method = {RequestMethod.GET, RequestMethod.POST})
 	public @ResponseBody
 	BaseOutput resume(ScheduleJob scheduleJob) {
--- ss-mvc/src/main/java/com/dili/ss/mvc/controller/CommonController.java
+++ ss-mvc/src/main/java/com/dili/ss/mvc/controller/CommonController.java
@@ -11,7 +11,6 @@ import com.dili.ss.service.CommonService;
 import com.dili.ss.util.POJOUtils;
 import com.dili.ss.util.ReflectionUtils;
 import com.github.pagehelper.Page;
-import io.swagger.annotations.Api;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.poi.ss.formula.functions.T;
 import org.slf4j.Logger;
@@ -31,8 +30,9 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.List;
 
-
-@Api(value = "common-api", description = "通用控制器操作", position = 1)
+/**
+ * 通用控制器操作
+ */
 @Controller
 @ConditionalOnBean(name = "commonMapper")
 @RequestMapping("/common")
