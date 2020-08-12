e5275b2cba51b0965a421661b72cd2524d52b2b2
wangmi <87730066>
2020-08-12 15:52:18 +0800

    3.6.8 优化全局异常拦截处理

--- ss-mvc/src/main/java/com/dili/ss/mvc/boot/GlobalExceptionHandler.java
+++ ss-mvc/src/main/java/com/dili/ss/mvc/boot/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.dili.ss.mvc.boot;
 import com.alibaba.fastjson.JSON;
 import com.dili.ss.domain.BaseOutput;
 import com.dili.ss.util.SpringUtil;
+import jdk.nashorn.internal.runtime.regexp.joni.exception.InternalException;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -52,6 +53,24 @@ public class GlobalExceptionHandler {
 //        return JSON.toJSONString(BaseOutput.failure(e.getMessage()));
 //    }
 
+    @ExceptionHandler(InternalException.class)
+    public String internalExceptionHandler(HttpServletRequest request, HttpServletResponse response, InternalException e) throws IOException {
+        e.printStackTrace();
+        //判断请求类型是json
+        if(request.getHeader("content-type").equals("application/json")){
+            response.setContentType("application/json;charset=UTF-8");
+            response.getWriter().write(JSON.toJSONString(BaseOutput.failure(e.getMessage())));
+            return null;
+        }
+        //判断是否是ajax访问，不是则跳到默认错误页面
+        if (request.getHeader("X-Requested-With") == null) {
+            request.setAttribute("exception", e);
+            return SpringUtil.getProperty("error.page.default", "error/default");
+        }
+        response.setContentType("application/json;charset=UTF-8");
+        return JSON.toJSONString(BaseOutput.failure(e.getMessage()));
+    }
+
     /**
      * 全局异常处理
      * @param e
@@ -59,15 +78,29 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(Exception.class)
     public String defultExcepitonHandler(HttpServletRequest request, HttpServletResponse response, Exception e) throws IOException {
+        Exception exception = e;
         e.printStackTrace();
-        String requestType = request.getHeader("X-Requested-With");
-        if (requestType == null) {
-            request.setAttribute("exception", e);
+        String exMsg = exception.getMessage();
+        //Sentinel限流引发的异常
+        if(e.getCause() != null && "com.alibaba.csp.sentinel.slots.block.flow.FlowException".equals(e.getCause().toString())){
+            exception = (Exception)e.getCause();
+            exMsg = "服务开启限流保护,请稍后再试!";
+        }
+        //判断请求类型是json
+        if(request.getHeader("content-type").equals("application/json")){
+            response.setContentType("application/json;charset=UTF-8");
+            response.getWriter().write(JSON.toJSONString(BaseOutput.failure(exMsg)));
+            return null;
+        }
+        //判断是否是ajax访问，不是则跳到默认错误页面
+        if (request.getHeader("X-Requested-With") == null) {
+            request.setAttribute("exception", exception);
+            request.setAttribute("exMsg", exMsg);
 //            response.sendRedirect(basePath + SpringUtil.getProperty("error.page.default", "error/default"));
             return SpringUtil.getProperty("error.page.default", "error/default");
         }
         response.setContentType("application/json;charset=UTF-8");
-        response.getWriter().write(JSON.toJSONString(BaseOutput.failure(e.getMessage())));
+        response.getWriter().write(JSON.toJSONString(BaseOutput.failure(exMsg)));
         return null;
     }
 
