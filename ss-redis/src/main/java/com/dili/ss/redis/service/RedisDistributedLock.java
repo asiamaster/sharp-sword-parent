9cf82e75c17dfd9c893539fc10c610be8ae3ccaa
wangmi <87730066>
2021-01-11 15:19:40 +0800

    3.7.1 ss-redis: 新增tryGetLockSync两个重载方法

--- ss-redis/src/main/java/com/dili/ss/redis/service/RedisDistributedLock.java
+++ ss-redis/src/main/java/com/dili/ss/redis/service/RedisDistributedLock.java
@@ -2,7 +2,6 @@ package com.dili.ss.redis.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.data.redis.connection.RedisStringCommands;
 import org.springframework.data.redis.connection.ReturnType;
@@ -11,7 +10,10 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.types.Expiration;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
 import java.nio.charset.Charset;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
 
 /**
  * redis分布式锁
@@ -24,13 +26,11 @@ public class RedisDistributedLock{
 
     private final Logger logger = LoggerFactory.getLogger(RedisDistributedLock.class);
 
-    @Autowired
-    private RedisTemplate redisTemplate;
+    protected RedisTemplate redisTemplate;
 
     public static final String UNLOCK_LUA;
 
     static {
-//        String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
         StringBuilder sb = new StringBuilder();
         sb.append("if redis.call(\"get\",KEYS[1]) == ARGV[1] ");
         sb.append("then ");
@@ -39,6 +39,15 @@ public class RedisDistributedLock{
         sb.append("    return 0 ");
         sb.append("end ");
         UNLOCK_LUA = sb.toString();
+    }
+
+    public RedisTemplate getRedisTemplate() {
+        return redisTemplate;
+    }
+
+    @Resource
+    public void setRedisTemplate(RedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
     }
 
     /**
@@ -62,42 +71,19 @@ public class RedisDistributedLock{
     }
 
     public Object get(String key) {
-        //下面的代码有bug，字符串两边会有双引号
-//        try {
-//            RedisCallback<String> callback = (connection) -> {
-//                JedisCommands commands = (JedisCommands) connection.getNativeConnection();
-//                return commands.get(key);
-//            };
-//            return redisTemplate.execute(callback);
-//        } catch (Exception e) {
-//            logger.error("get redis occured an exception", e);
-//            return null;
-//        }
         return redisTemplate.opsForValue().get(key);
     }
 
     /**
-     * 获取锁，key作为获取锁的条件，key和value将作为释放锁的条件
+     * 获取锁，未获取或异常到返回false
+     * key作为获取锁的条件，key和value将作为释放锁的条件
      * @param key
      * @param value
      * @param expire 单位秒
      * @return  获取失败返回false
      */
     public boolean tryGetLock(String key, String value,  long expire) {
-//        try {
-//            RedisCallback<String> callback = (connection) -> {
-//                JedisCommands commands = (JedisCommands) connection.getNativeConnection();
-//                return commands.set(key, value, "NX", "PX", expire);
-//            };
-//            String result = redisTemplate.execute(callback);
-//            return !StringUtils.isEmpty(result);
-//        } catch (Exception e) {
-//            logger.error("set redis occured an exception", e);
-//        }
-//        return false;
         try {
-//            ValueOperations<String, Object> operations = redisTemplate.opsForValue();
-//            return operations.setIfAbsent(key, value, expire, TimeUnit.SECONDS);
             RedisCallback<Boolean> callback = (connection) -> {
                 return connection.set(key.getBytes(Charset.forName("UTF-8")), value.getBytes(Charset.forName("UTF-8")), Expiration.seconds(expire), RedisStringCommands.SetOption.SET_IF_ABSENT);
             };
@@ -108,59 +94,60 @@ public class RedisDistributedLock{
         }
     }
 
-//    /**
-//     * 获取分布式锁，原子操作
-//     * Lettuces
-//     * @param lockKey
-//     * @param requestId 唯一ID, 可以使用UUID.randomUUID().toString();
-//     * @param expire
-//     * @param timeUnit
-//     * @return
-//     */
-//    public boolean tryLock(String lockKey, String requestId, long expire, TimeUnit timeUnit) {
-//        try{
-//            RedisCallback<Boolean> callback = (connection) -> {
-//                return connection.set(lockKey.getBytes(Charset.forName("UTF-8")), requestId.getBytes(Charset.forName("UTF-8")), Expiration.seconds(timeUnit.toSeconds(expire)), RedisStringCommands.SetOption.SET_IF_ABSENT);
-//            };
-//            return (Boolean)redisTemplate.execute(callback);
-//        } catch (Exception e) {
-//            log.error("redis lock error.", e);
-//        }
-//        return false;
-//    }
+    /**
+     * 同步获取锁，异常时返回false
+     * key作为获取锁的条件，key和value将作为释放锁的条件
+     * 锁无限等待，每次获取失败后睡眠10毫秒
+     * @param key
+     * @param value
+     * @param expire 单位秒
+     * @return  获取失败返回false
+     */
+    public boolean tryGetLockSync(String key, String value, long expire) {
+        return tryGetLockSync(key, value, expire, Long.MAX_VALUE, TimeUnit.SECONDS, Long.MAX_VALUE, 10L, TimeUnit.MILLISECONDS);
+    }
 
-    //Jedis方式
-//    public boolean releaseLock(String key,String requestId) {
-//        // 释放锁的时候，有可能因为持锁之后方法执行时间大于锁的有效期，此时有可能已经被另外一个线程持有锁，所以不能直接删除
-//        try {
-//            //由于在RedisCacheConfig中配置了redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
-//            //所以redis中的值在两边有引号，对比值时需要在两边加上引号
-//            final String value = "\""+requestId+"\"";
-//            // 使用lua脚本删除redis中匹配value的key，可以避免由于方法执行时间过长而redis锁自动过期失效的时候误删其他线程的锁
-//            // spring自带的执行脚本方法中，集群模式直接抛出不支持执行脚本的异常，所以只能拿到原redis的connection来执行脚本
-//            RedisCallback<Long> callback = (connection) -> {
-//                Object nativeConnection = connection.getNativeConnection();
-//                // 集群模式和单机模式虽然执行脚本的方法一样，但是没有共同的接口，所以只能分开执行
-//                // 集群模式
-//                if (nativeConnection instanceof JedisCluster) {
-//                    return (Long) ((JedisCluster) nativeConnection).eval(UNLOCK_LUA, Collections.singletonList(key), Collections.singletonList(value));
-//                }
-//                // 单机模式
-//                else if (nativeConnection instanceof Jedis) {
-//                    return (Long) ((Jedis) nativeConnection).eval(UNLOCK_LUA, Collections.singletonList(key), Collections.singletonList(value));
-//                }
-//                return 0L;
-//            };
-//            Long result = (Long)redisTemplate.execute(callback);
-//            return result != null && result > 0;
-//        } catch (Exception e) {
-//            logger.error("release lock occured an exception", e);
-//        } finally {
-//            // 清除掉ThreadLocal中的数据，避免内存溢出
-//            //lockFlag.remove();
-//        }
-//        return false;
-//    }
+
+
+    /**
+     * 同步获取锁，异常时返回false
+     * key作为获取锁的条件，key和value将作为释放锁的条件
+     * @param key
+     * @param value
+     * @param expire 锁过期时间, 单位秒
+     * @param awaitTime  锁等待超时时间
+     * @param awaitUnit  锁等待超时时间单位
+     * @param retryCount 最大获取次数
+     * @param sleepTime  每次获取失败后的睡眠时间
+     * @param sleepUnit  每次获取失败后的睡眠时间单位
+     * @return  获取失败返回false
+     */
+    public boolean tryGetLockSync(String key, String value, long expire, long awaitTime, TimeUnit awaitUnit, long retryCount, long sleepTime, TimeUnit sleepUnit) {
+        if(tryGetLock(key, value, expire)){
+            return true;
+        }
+        //不行再获取
+        long nanos = awaitUnit.toNanos(awaitTime);
+        final long deadline = System.nanoTime() + nanos;
+        int count = 0;
+        while (true) {
+            nanos = deadline - System.nanoTime();
+            //超时
+            if (nanos <= 0L) {
+                return false;
+            }
+            if (tryGetLock(key, value, expire)) {
+                return true;
+            }
+            //如果大于最大获取次数或者线程被中断
+            if (count++ > retryCount || Thread.interrupted()) {
+                return false;
+            }
+            //阻塞
+            LockSupport.parkNanos(sleepUnit.toNanos(sleepTime));
+        }
+    }
+
 
     /**
      * Lettuce方式释放锁
