07b8c30f1cf2bff2a90603382f6e29d18c6a5e36
wangmi <87730066>
2021-02-26 10:02:51 +0800

    3.7.3 spring boot 从2.4.2降到2.3.9.RELEASE

--- ss-mbg/src/test/java/com/dili/ss/test/GeneratorTest.java
+++ ss-mbg/src/test/java/com/dili/ss/test/GeneratorTest.java
@@ -1,15 +1,13 @@
 package com.dili.ss.test;
 
 import com.dili.ss.mbg.ProjectGenerator;
-import org.junit.Test;
 
 /**
  * Created by asiamaster on 2017/6/29 0029.
  */
 public class GeneratorTest {
 
-	@Test
-	public void generateDefault() throws Exception {
+	public void main(String[] args) throws Exception {
 		ProjectGenerator.generate("E:\\workspace\\dili-workspace\\ms-workspace\\sharp-sword-parent\\ss-mbg\\src\\main\\resources\\generator\\sharpGeneratorConfig.xml");
 	}
 
